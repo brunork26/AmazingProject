43d05169009dfb291dd5ab12b0e86babd0d947bd
hugonn <hugoneto@Outlook.com>
2018-10-02 22:05:37 -0300

    Add primeira parte tst

--- AmazingProject/src/app/app.module.ts
+++ AmazingProject/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CadastroMedicoComponent } from './cadastro-medico/cadastro-medico.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroMedicoComponent
   ],
   imports: [
     BrowserModule
