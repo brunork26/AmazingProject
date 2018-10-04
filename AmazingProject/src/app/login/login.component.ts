e94494409d8511015d8402adf75d17044c3189f2
hugonn <hugoneto@Outlook.com>
2018-10-04 18:11:29 -0300

    Add layout

--- AmazingProject/src/app/login/login.component.ts
+++ AmazingProject/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FormControl, Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { AuthService } from '../shared-service/auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-
+  public form2: FormGroup;
   ngOnInit() {
   }
 
+  
+  getErrorMessage() {
+    return this.form2.get('email').hasError('required') ? 'Digite seu E-mail' :
+           this.form2.get('email').hasError('email') ? 'Email inválido' :
+            '';
+  }
+
+  constructor(private authService: AuthService,
+              private formBuilder: FormBuilder) {
+              this.form2 = this.formBuilder.group({
+                email: new FormControl('', [Validators.required, Validators.email]),
+                password: new FormControl('', [Validators.required])
+              });
+   }
+   tryLogin(){
+     alert("Logando...");
+   }
 }
